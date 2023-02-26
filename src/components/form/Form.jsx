99fe3e1d4854df14b87c725346fb5423b685827d
Maryna Karamanova <karamanova91@gmail.com>
2023-02-26 16:20:40 +0200

    deleted phone and number from state in App

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 import { AddButton, FormContainer, NameInput, PhoneInput } from './Form.styled';
 const initialState = {
     name: '',
-    phone: '',
+    number: '',
 }
 export class Form extends Component {
     state = initialState
@@ -14,17 +14,17 @@ export class Form extends Component {
     }
     handleFormSubmit = (e) => {
         e.preventDefault()
-        const { name, phone } = this.state;
+        const { name, number } = this.state;
         const { onAdd } = this.props;
         const isValidatedForm = this.validatedForm()
         if (!isValidatedForm) return
-        onAdd({ id: nanoid(), name, phone })
+        onAdd({ id: nanoid(), name, number })
         this.resetForm()
     }
     validatedForm = () => {
-        const { name, phone } = this.state;
+        const { name, number } = this.state;
         const { onCheck } = this.props;
-        if (!name || !phone) {
+        if (!name || !number) {
             alert('The field is empty')
             return false
         }
@@ -32,16 +32,16 @@ export class Form extends Component {
     }
     resetForm = () => this.setState(initialState)
     render() {
-        const {name, phone} = this.state
+        const {name, number} = this.state
         return (
         <FormContainer onSubmit={this.handleFormSubmit}>
                 <NameInput type="text" name="name" placeholder="Enter name" pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
   required value={name} onChange={this.handleChangeForm}>
             </NameInput>
-            <PhoneInput type="tel" name="phone" placeholder="Enter phone number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            <PhoneInput type="tel" name="number" placeholder="Enter phone number" pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-  required value={phone} onChange={this.handleChangeForm}>
+  required value={number} onChange={this.handleChangeForm}>
             </PhoneInput>
         <AddButton type="submit">Add Contact</AddButton>
         </FormContainer >
